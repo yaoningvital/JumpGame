16cd7c5f270bb1c0d1025723d77b3cd783162e59
姚宁 <ning.yao@ikang.com>
2019-11-17 20:29:07 +0800

    修改了棋子的颜色，使之看上去更自然

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -4,15 +4,16 @@ class Circle extends React.Component {
   render () {
     let {circleData, handleClickCircle, r, a, boardRowHeight} = this.props
     
+    let backgroundImage = `radial-gradient(at 80px 80px, rgba(0,0,0,0), ${circleData.color})`
     let buttonWidth = 2 * r
-    let buttonMargin = `${(boardRowHeight-buttonWidth)/2}px ${a / 2}px`
+    let buttonMargin = `${(boardRowHeight - buttonWidth) / 2}px ${a / 2}px`
     
     let boxShadow = circleData.color === '#ddd' ?
-      '2px 2px 2px #888888 inset' : '3px 3px 2px #bbb'
+      '2px 2px 2px #888888 inset' : '3px 3px 2px rgba(0,0,0,0.2)'
     return (
       <button
         style={{
-          backgroundColor: circleData.color,
+          backgroundImage: backgroundImage,
           boxShadow: boxShadow,
           width: buttonWidth + 'px',
           height: buttonWidth + 'px',
