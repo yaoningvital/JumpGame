9ea19ee074597c4b83e3f65912d3299ccae86d0e
姚宁 <ning.yao@ikang.com>
2019-11-23 23:10:48 +0800

    历史步骤给一个固定高度

--- src/components/operate-area/HistorySteps.js
+++ src/components/operate-area/HistorySteps.js
@@ -3,19 +3,21 @@ import React from 'react'
 function HistorySteps (props) {
   let {history, handleStepBackTo} = props
   return (
-    <div>
+    <div className="history-steps">
       <h4>历史步骤：</h4>
-      {
-        history.map((step, move) => {
-          let desc = move ? `回退到第${move}步` : '重新开始游戏'
-          return (
-            <button
-              key={move}
-              onClick={() => handleStepBackTo(move)}
-            >{desc}</button>
-          )
-        })
-      }
+      <div>
+        {
+          history.map((step, move) => {
+            let desc = move ? `回退到第${move}步` : '重新开始游戏'
+            return (
+              <button
+                key={move}
+                onClick={() => handleStepBackTo(move)}
+              >{desc}</button>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
